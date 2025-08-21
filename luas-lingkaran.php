17e7dc194f7a3680b50a12a9e6167e86c63bd5a1
Rara <wwa751623@gmail.com>
2025-08-21 11:45:28 +0700

    luas-lingkaran

--- luas-lingkaran.php
+++ luas-lingkaran.php
@@ -1,4 +1,11 @@
 <?php
+// Soal
+echo "<h2>Soal Luas Lingkaran</h2>";
 $r = 7;
-$luas = 3.14159 * $r * $r;
-echo "Luas lingkaran (r=$r) = $luas";
+echo "Hitunglah luas lingkaran dengan jari-jari = $r <br>";
+
+// Jawaban
+$luas = 3.14 * $r * $r;
+echo "<h3>Jawaban:</h3>";
+echo "Luas = π × r² = 3.14 × $r × $r = $luas";
+?>
