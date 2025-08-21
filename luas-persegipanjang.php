2b9d2151f815df36db2e9e2da7a533b9a0b16a57
Rara <wwa751623@gmail.com>
2025-08-21 11:50:07 +0700

    luas-persegipanjang

--- luas-persegipanjang.php
+++ luas-persegipanjang.php
@@ -1,4 +1,12 @@
 <?php
-$p = 12; $l = 5;
+// Soal
+echo "<h2>Soal Luas Persegi Panjang</h2>";
+$p = 12;
+$l = 5;
+echo "Hitunglah luas persegi panjang dengan panjang = $p dan lebar = $l <br>";
+
+// Jawaban
 $luas = $p * $l;
-echo "Luas persegi panjang (p=$p, l=$l) = $luas";
+echo "<h3>Jawaban:</h3>";
+echo "Luas = panjang × lebar = $p × $l = $luas";
+?>
