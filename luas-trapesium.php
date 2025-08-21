cc3e4959daa87c46c3eeb1653d976bef5158140b
Rara <wwa751623@gmail.com>
2025-08-21 11:51:44 +0700

    luas-trapesium

--- luas-trapesium.php
+++ luas-trapesium.php
@@ -1,4 +1,13 @@
 <?php
-$a = 10; $b = 14; $t = 6; // a dan b sisi sejajar
+// Soal
+echo "<h2>Soal Luas Trapesium</h2>";
+$a = 10; // sisi sejajar 1
+$b = 6;  // sisi sejajar 2
+$t = 4;  // tinggi
+echo "Hitunglah luas trapesium dengan sisi sejajar a = $a, b = $b dan tinggi = $t <br>";
+
+// Jawaban
 $luas = 0.5 * ($a + $b) * $t;
-echo "Luas trapesium (a=$a, b=$b, t=$t) = $luas";
+echo "<h3>Jawaban:</h3>";
+echo "Luas = 1/2 × (a + b) × t = 0.5 × ($a + $b) × $t = $luas";
+?>
