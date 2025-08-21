59f215709182ca282bccab92dd18aa22358c74f5
Rara <wwa751623@gmail.com>
2025-08-21 11:48:29 +0700

    luas-persegi

--- luas-persegi.php
+++ luas-persegi.php
@@ -1,4 +1,12 @@
 <?php
-$s = 6;
-$luas = $s * $s;
-echo "Luas persegi (s=$s) = $luas";
+// Soal
+echo "<h2>Soal Luas Persegi</h2>";
+$sisi = 8;
+echo "Hitunglah luas persegi dengan sisi = $sisi <br>";
+
+// Jawaban
+$luas = $sisi * $sisi;
+echo "<h3>Jawaban:</h3>";
+echo "Luas = sisi × sisi = $sisi × $sisi = $luas";
+?>
+
