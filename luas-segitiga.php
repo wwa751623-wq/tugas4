d09661ac65fd4bdcbd4c8eb3e9cc2a5edbf23205
Rara <wwa751623@gmail.com>
2025-08-21 11:47:04 +0700

    luas-segitiga

--- luas-segitiga.php
+++ luas-segitiga.php
@@ -1,4 +1,12 @@
 <?php
-$alas = 10; $tinggi = 8;
+// Soal
+echo "<h2>Soal Luas Segitiga</h2>";
+$alas = 10;
+$tinggi = 6;
+echo "Hitunglah luas segitiga dengan alas = $alas dan tinggi = $tinggi <br>";
+
+// Jawaban
 $luas = 0.5 * $alas * $tinggi;
-echo "Luas segitiga (alas=$alas, tinggi=$tinggi) = $luas";
+echo "<h3>Jawaban:</h3>";
+echo "Luas = 1/2 × alas × tinggi = 0.5 × $alas × $tinggi = $luas";
+?>
